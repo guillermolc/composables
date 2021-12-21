de0759181b2440642f610a1443c83ff5b75a7b3a
guillermo <guillermo.lagos05@gmail.com>
2021-12-21 11:09:53 -0300

    Adding GlShapes and GlProperties for set default theme properties

---
+++ shared/src/main/java/guillermo/lagos/composables/shared/GlShapes.kt
@@ -0,0 +1,9 @@
+package guillermo.lagos.composables.shared
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+object GlShapes {
+
+    val Rounded = RoundedCornerShape(GlProperties.radius)
+
+}
